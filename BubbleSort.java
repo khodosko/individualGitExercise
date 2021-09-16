9c05a5cc6ba3b702d26bd7d466e4fbe473803062
khodosko <dkhodosko@gmail.com>
2021-09-16 15:38:45 -0700

    commit

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+public class BubbleSort{
+    public static void main(String[] args) {
+        //Create array 
+        int arr [] = new int[100];
+        //populate with random values
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random()*10);
+        }
+        //print unsorted array
+        System.out.println("Unsorted array");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +  " ");
+        }
+        bubbleSort(arr);
+        System.out.println("");
+        //print sorted array
+        System.out.println("Sorted array");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +  " ");
+        }
+    }
+    
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length -1 - i; j++) {
+                if (arr[j+1] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
